65ad7c9358318c55d83844070c043a78b8dde855
David Andersson <David Andersson>
2019-05-02 11:25:28 +0200

    Simple example with button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Funktionell komponent:
+// const App = () => {
+// 	const [value, setValue] = useState(1);  // state
+// 	const handleClick = event => {
+// 		console.log('handleClick anropad med parametern:', event);
+// 		setValue(value + 1);
+// 	}
+// 	return (
+// 		<div className="App">
+// 			The value is: {value} <br/>
+// 			<button onClick={handleClick}>Click me</button>
+// 		</div>
+// 	);
+// };
+
+// Klassbaserad komponent:
+class App extends React.Component {
+	state = {
+		value: 1
+	}
+	handleClick = event => {
+		console.log('handleClick anropad med parametern:', event);
+		this.setState({
+			value: this.state.value + 1
+		});
+	}
+	render() {
+		return (
+			<div className="App">
+				The value is: {this.state.value} <br/>
+				<button onClick={this.handleClick}>Click me</button>
+			</div>
+		);
+	}
 }
 
 export default App;
